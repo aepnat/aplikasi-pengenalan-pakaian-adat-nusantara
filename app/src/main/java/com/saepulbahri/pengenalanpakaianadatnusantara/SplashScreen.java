2bdd384223b321d80ce32bae05ea3ec3338066c3
aepnat <aep.nat@gmail.com>
2019-12-10 00:39:50 +0700

    initial commit

---
+++ app/src/main/java/com/saepulbahri/pengenalanpakaianadatnusantara/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.saepulbahri.pengenalanpakaianadatnusantara;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    TextView tvSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        setContentView(R.layout.activity_splash_screen);
+
+        tvSplash = (TextView) findViewById(R.id.tvSplash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L);
+    }
+}
