6a23aca5a30f72def8b5a06b0e13df70295f39a1
aepnat <aep.nat@gmail.com>
2020-01-03 16:37:01 +0700

    scroll view on quiz

--- app/src/main/java/com/lina/pengenalanpakaianadatnusantaran/KategoriGameActivity.java
+++ app/src/main/java/com/lina/pengenalanpakaianadatnusantaran/KategoriGameActivity.java
@@ -1,8 +1,11 @@
 package com.lina.pengenalanpakaianadatnusantaran;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class KategoriGameActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class KategoriGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kategori_game);
+
+        CardView cardGamePakaianAdat = findViewById(R.id.cardGamePakaianAdat);
+        cardGamePakaianAdat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(KategoriGameActivity.this, GamePakaianAdatActivity.class);
+                KategoriGameActivity.this.startActivity(intent);
+            }
+        });
     }
 }
